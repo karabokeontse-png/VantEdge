d702ec66b3106b5c4afbba7d53c42cc13a443d94
VantEdge Recovery <recovery@vantedge.io>
2026-06-15 10:51:24 +0000

    feat: recover viewmodel state/event sealed classes (11 files)
    Reconstruct ActionType, CycleNavEvent, CycleStage, ExportEvent, ExportState,
    JobExtractionState, OnboardingExtractionState, RestorationState, ResultError,
    WorkflowState as idiomatic Kotlin sealed/enum/data classes.
    Delete duplicate AtsResult.kt in engine dir (already in AtsEngine.kt).

--- app/src/main/java/com/vantedge/app/data/viewmodel/ActionType.kt
+++ app/src/main/java/com/vantedge/app/data/viewmodel/ActionType.kt
@@ -1,13 +1,6 @@
-package com.vantedge.app.data.viewmodel;
+package com.vantedge.app.data.viewmodel
 
-import kotlin.Metadata;
-import kotlin.enums.EnumEntries;
-import kotlin.enums.EnumEntriesKt;
-
-/* compiled from: CycleViewModel.kt */
-@Metadata(d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0010\n\u0002\b\n\b\u0086\u0081\u0002\u0018\u00002\b\u0012\u0004\u0012\u00020\u00000\u0001B\u0007\b\u0002¢\u0006\u0002\u0010\u0002j\u0002\b\u0003j\u0002\b\u0004j\u0002\b\u0005j\u0002\b\u0006j\u0002\b\u0007j\u0002\b\bj\u0002\b\tj\u0002\b\n¨\u0006\u000b"}, d2 = {"Lcom/vantedge/app/data/viewmodel/ActionType;", "", "(Ljava/lang/String;I)V", "SAVE_ANALYSIS", "DISMISS", "CONTINUE_TO_CV", "SAVE_APPLICATION", "EXPORT", "IMPROVE", "COPY", "SAVE_PDF", "app_debug"}, k = 1, mv = {1, 9, 0}, xi = 48)
-/* loaded from: classes9.dex */
-public enum ActionType {
+enum class ActionType {
     SAVE_ANALYSIS,
     DISMISS,
     CONTINUE_TO_CV,
@@ -15,11 +8,5 @@ public enum ActionType {
     EXPORT,
     IMPROVE,
     COPY,
-    SAVE_PDF;
-
-    private static final /* synthetic */ EnumEntries $ENTRIES = EnumEntriesKt.enumEntries($VALUES);
-
-    public static EnumEntries<ActionType> getEntries() {
-        return $ENTRIES;
-    }
+    SAVE_PDF
 }
--- app/src/main/java/com/vantedge/app/data/viewmodel/CycleStage.kt
+++ app/src/main/java/com/vantedge/app/data/viewmodel/CycleStage.kt
@@ -1,21 +1,8 @@
-package com.vantedge.app.data.viewmodel;
+package com.vantedge.app.data.viewmodel
 
-import kotlin.Metadata;
-import kotlin.enums.EnumEntries;
-import kotlin.enums.EnumEntriesKt;
-
-/* compiled from: CycleViewModel.kt */
-@Metadata(d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0010\n\u0002\b\u0006\b\u0086\u0081\u0002\u0018\u00002\b\u0012\u0004\u0012\u00020\u00000\u0001B\u0007\b\u0002¢\u0006\u0002\u0010\u0002j\u0002\b\u0003j\u0002\b\u0004j\u0002\b\u0005j\u0002\b\u0006¨\u0006\u0007"}, d2 = {"Lcom/vantedge/app/data/viewmodel/CycleStage;", "", "(Ljava/lang/String;I)V", "ANALYZED", "READY_FOR_GENERATION", "DOCUMENTS_GENERATED", "IMPROVEMENT_ACTIVE", "app_debug"}, k = 1, mv = {1, 9, 0}, xi = 48)
-/* loaded from: classes9.dex */
-public enum CycleStage {
+enum class CycleStage {
     ANALYZED,
     READY_FOR_GENERATION,
     DOCUMENTS_GENERATED,
-    IMPROVEMENT_ACTIVE;
-
-    private static final /* synthetic */ EnumEntries $ENTRIES = EnumEntriesKt.enumEntries($VALUES);
-
-    public static EnumEntries<CycleStage> getEntries() {
-        return $ENTRIES;
-    }
+    IMPROVEMENT_ACTIVE
 }
